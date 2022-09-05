92f852ed8c645ef58c85af1b84e36ef5a29e145d
SeeYou1ater <Sergeyharechkov8@mail.ru>
2022-09-05 15:25:27 +0300

    add RTK add and delete flow

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import { useGetGoodsQuery } from './redux/goodsAPI';
+import { useAddProductMutation, useDeleteProductMutation, useGetGoodsQuery } from './redux/goodsAPI';
 import './App.css';
 import { useState } from 'react';
 
 function App() {
   const [count, setCount] = useState('')
+  const [newProduct, setNewProduct] = useState('')
   const { data = [], isLoading } = useGetGoodsQuery(count)
+  const [addProduct, { isError }] = useAddProductMutation()
+  const [deleteProduct] = useDeleteProductMutation()
+
+  const handleAddProduct = async () => {
+    if (newProduct) {
+      await addProduct({ name: newProduct }).unwrap()
+      setNewProduct('')
+    }
+  }
+
+  const handleDeleteProduct = async (id) => {
+      await deleteProduct(id).unwrap()
+  }
 
   if (isLoading) { return <h1>Loading...</h1> }     
 
   return (
     <div className="App">
       <div>
+       <input type="text" value={newProduct} onChange={(e) => { setNewProduct(e.target.value) }}/>
+       <button onClick={handleAddProduct}>Add Product</button>   
+      </div> 
+      <div>
         <select value={count} onChange={ (e) => { setCount(e.target.value) }}>
-          <option value="''">all</option>
+          <option value="">all</option>
           <option value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
         </select>  
       </div>
       <ul>
-        { data.map( item => <li key={item.id}>{item.name}</li> ) }
+        { data.map( item => <li onClick={() => { handleDeleteProduct(item.id) }} key={item.id}>{item.name}</li> ) }
       </ul>
     </div>
-  );
+  )
 }
 
 export default App;
