531ca558001bc3866c8bc8296c6c4eee2535def6
SeeYou1ater <Sergeyharechkov8@mail.ru>
2022-09-05 15:25:51 +0300

    add RTK add and delete flow

--- src/redux/goodsAPI.js
+++ src/redux/goodsAPI.js
@@ -3,11 +3,31 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const goodsAPI = createApi({
   reducerPath: 'goodsApi',
   baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:3002/'}),
+  tagTypes: ['Products'],
   endpoints: (build) => ({
     getGoods: build.query({
       query: (limit = '') => `goods?${ limit && `_limit=${limit}`}`,
+      providesTags: (result) =>
+      result
+        ? [...result.map(({ id }) => ({ type: 'Products', id })), { type: 'Products', id: 'LIST' }]
+        : [{type: 'Products', id: 'LIST'}]
+    }),
+    addProduct: build.mutation({
+      query: (body) => ({
+        url: 'goods',
+        method: 'POST',
+        body
+      }),
+      invalidatesTags: [{type: 'Products', id: 'LIST'}]
+    }),
+    deleteProduct: build.mutation({
+      query: (id) => ({
+        url: `goods/${id}`,
+        method: 'DELETE'
+      }),
+      invalidatesTags: [{type: 'Products', id: 'LIST'}]
     })
   })
 })
 
-export const { useGetGoodsQuery } = goodsAPI;
+export const { useGetGoodsQuery, useAddProductMutation, useDeleteProductMutation } = goodsAPI;
